8bebf53b333e45c1651b6bc047f04b66b95ba600
Eugene Obrezkov <ghaiklor@gmail.com>
2015-05-15 17:41:33 +0300

    Some thoughts in tests

--- test/BaseStorage.test.js
+++
@@ -1,8 +0,0 @@
-var assert = require('chai').assert;
-var BaseStorage = require('../lib/BaseStorage');
-
-describe('BaseStorage', function () {
-  it('Should properly export Base Storage', function () {
-    assert.isFunction(BaseStorage);
-  });
-});
--- test/StorageFactory.test.js
+++ test/StorageFactory.test.js
@@ -2,7 +2,12 @@ var assert = require('chai').assert;
 var StorageFactory = require('../lib/StorageFactory');
 
 describe('StorageFactory', function () {
-  it('Should properly export Storage Factory Service', function () {
+  it('Should properly export Storage Factory', function () {
     assert.isFunction(StorageFactory);
+  });
+
+  it('Should properly create Storage Factory', function () {
+    var storage = new StorageFactory();
+    assert.instanceOf(storage, StorageFactory);
   });
 });
