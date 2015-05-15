4998299a1e50027a1f1ac1ec8119e20b7eeee0a0
Eugene Obrezkov <ghaiklor@gmail.com>
2015-05-15 11:46:01 +0300

    Move back to ES5

--- test/BaseStorage.test.js
+++ test/BaseStorage.test.js
@@ -1,5 +1,5 @@
 var assert = require('chai').assert;
-var BaseStorage = require('../src/BaseStorage.es6');
+var BaseStorage = require('../lib/BaseStorage');
 
 describe('BaseStorage', function () {
   it('Should properly export class', function () {
