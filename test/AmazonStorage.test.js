360af5135f424f1154341ef2a0310d55f640f732
Eugene Obrezkov <ghaiklor@gmail.com>
2015-05-18 12:16:38 +0300

    Done with AmazonStorage

--- test/AmazonStorage.test.js
+++ test/AmazonStorage.test.js
@@ -1,24 +1,14 @@
 var assert = require('chai').assert;
 var AmazonStorage = require('../lib/AmazonStorage');
 
-var amazonConfig = {
-  accessKeyId: '1234',
-  secretAccessKey: '1234'
-};
-
 describe('AmazonStorage', function () {
   it('Should properly export Amazon Storage Service', function () {
     assert.isFunction(AmazonStorage);
   });
 
-  //it('Should properly throw exception', function () {
-  //  assert.throws(function () {
-  //    return new AmazonStorage();
-  //  }, Error);
-  //});
-
   it('Should properly create Amazon Storage', function () {
-    var storage = new AmazonStorage(amazonConfig);
+    var storage = new AmazonStorage();
     assert.instanceOf(storage, AmazonStorage);
+    assert.isFunction(storage.upload);
   });
 });
