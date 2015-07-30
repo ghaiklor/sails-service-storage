3a3985ce7d61fdcd732337667ee31589da2a30b2
Eugene Obrezkov <ghaiklor@gmail.com>
2015-07-30 12:02:14 +0300

    Fix tests for StorageService

--- test/index.js
+++ test/index.js
@@ -1,18 +1,15 @@
 var assert = require('chai').assert;
-var AmazonStorage = require('../lib/AmazonStorage');
-var GoogleCloudStorage = require('../lib/GoogleCloudStorage');
 var StorageService = require('../');
+var AmazonStorage = StorageService.AmazonStorage;
 
 describe('Factory Method', function () {
   it('Should properly export', function () {
     assert.isObject(StorageService);
     assert.isFunction(StorageService.create);
-    assert.isFunction(StorageService.Amazon);
-    assert.isFunction(StorageService.GoogleCloud);
+    assert.isFunction(StorageService.AmazonStorage);
   });
 
   it('Should properly create storage instances', function () {
-    assert.instanceOf(StorageService.create('Amazon'), AmazonStorage);
-    assert.instanceOf(StorageService.create('Google Cloud'), GoogleCloudStorage);
+    assert.instanceOf(StorageService.create('amazon'), AmazonStorage);
   });
 });
