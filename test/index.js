09a038dde6ea453fafad4899c43746e461c24a40
Eugene Obrezkov <ghaiklor@gmail.com>
2015-08-03 17:52:56 +0300

    Improve test coverage for StorageService

--- test/index.js
+++ test/index.js
@@ -12,4 +12,10 @@ describe('Factory Method', function () {
   it('Should properly create storage instances', function () {
     assert.instanceOf(StorageService.create('amazon'), AmazonStorage);
   });
+
+  it('Should properly throw error on unrecognized type', function () {
+    assert.throws(function () {
+      StorageService.create('NOT_EXISTS');
+    }, Error);
+  });
 });
