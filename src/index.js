ad9f16194609d1a9a21e30d183bfc106f88870e6
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-25 17:35:16 +0300

    Rewrite StorageService with ES6

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-var storage = {
-  amazon: require('./AmazonStorage')
+import AmazonStorage from './AmazonStorage';
+import LocalStorage from './LocalStorage';
+
+const storage = {
+  amazon: AmazonStorage,
+  local: LocalStorage
 };
 
-module.exports = {
-  /**
-   * Create specified storage instance
-   * @param {String} type
-   * @param {Object} config
-   * @returns {*}
-   */
-  create: function (type, config) {
-    if (storage[type.toLowerCase()] instanceof Function) {
-      return new storage[type.toLowerCase()](config);
-    } else {
-      throw new Error('Unrecognized storage type -> ' + type);
-    }
-  },
-
-  AmazonStorage: storage.amazon
-};
+/**
+ * Create specified storage instance
+ * @param {String} type
+ * @param {Object} config
+ * @returns {*}
+ */
+export default function (type, config) {
+  if (storage[type.toLowerCase()] instanceof Function) {
+    return new storage[type.toLowerCase()](config);
+  } else {
+    throw new Error('Unrecognized storage type -> ' + type);
+  }
+}
